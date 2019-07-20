604b2f8069e3af257b53ccfcc5cf6fe9ebf65999
Alexander Elfimow <my.free.mail@gmx.de>
2019-07-20 23:23:50 +0200

    src: added prefix

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <cmath>
+#include <stdexcept>
+#include <map>
 
 constexpr double c { 299792458.0 };
 constexpr double zero { 0.0 };
@@ -8,9 +10,9 @@ constexpr double zero { 0.0 };
 int main(int argc, char *argv[])
 try
 {
-    if (argc != 3)
+    if (argc != 4)
     {
-        throw std::invalid_argument("Usage: frequency epsilon");
+        throw std::invalid_argument("Usage: frequency prefix epsilon");
     }
 
     auto frequency { zero };
@@ -19,16 +21,40 @@ try
         ss >> frequency;
     }
 
-    auto epsilon { zero };
+    std::string prefix;
     {
         std::stringstream ss { argv[2] };
+        ss >> prefix;
+    }
+
+    auto epsilon { zero };
+    {
+        std::stringstream ss { argv[3] };
         ss >> epsilon;
     }
 
-    std::cout << "Frequency: " << frequency << std::endl;
+    std::map<std::string, double> supported_prefix
+    {
+        { "Hz", 1.0 },
+        { "kHz", 1.0E3 },
+        { "MHz", 1.0E6 },
+        { "GHz", 1.0E9 },
+        { "THz", 1.0E12 }
+    };
+
+    auto it { supported_prefix.find(prefix) };
+
+    if (it == supported_prefix.end())
+    {
+        throw std::runtime_error("Unsupported prefix: " + prefix);
+    }
+
+    auto const mult_value { it->second };
+
+    std::cout << "Frequency: " << frequency << " " << prefix << std::endl;
     std::cout << "Epsilon: " << epsilon << std::endl;
 
-    auto const lambda = c / (frequency * std::sqrt(epsilon));
+    auto const lambda = c / (frequency * mult_value * std::sqrt(epsilon));
 
     std::cout << "Wavelength: " << lambda << " m" << std::endl;
 
